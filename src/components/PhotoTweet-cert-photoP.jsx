e34623bda5e72de22c48ee36fa4578d14531dfd6
betsaleel yoto <betsaleelyoto@gmail.com>
2024-01-15 17:55:23 +0200

    Trosième commit

---
+++ src/components/PhotoTweet-cert-photoP.jsx
@@ -0,0 +1,20 @@
+function PhotoPCertifNomProfile() {
+  return (
+    <>
+      <div className="photoTweet_certif">
+        <div className="photo_pt">
+          <img src="" alt="" />
+        </div>
+        <p>
+          CNN
+          <span className="certififie">
+            <img src="../src/Icons/Verified.svg" alt="" />
+          </span>
+          @CNN.7m
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default PhotoPCertifNomProfile;
